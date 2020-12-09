25206e519c98a6269a6e0c29a69ae869f56fd4b8
pushpsenairekar2911 <pushpsenairekar@Pushpsens-Mac-mini.local>
2020-12-09 15:32:15 +0530

    v2.1.9

--- CometChatSwift/CometChatSwift/AppDelegate.swift
+++ CometChatSwift/CometChatSwift/AppDelegate.swift
@@ -60,7 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-        CometChat.startServices()
+        self.initialization()
         
     }
     
@@ -69,11 +69,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
-        CometChat.startServices()
+        self.initialization()
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
-        CometChat.startServices()
+        self.initialization()
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
     
